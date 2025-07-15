bd09c5524ba34a005ce835efa8fbc44545eb4256
notlemos <caiolemossfc123@hotmail.com>
2025-07-15 20:14:55 -0300

    Criando Stream de Leitura

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -9,6 +9,36 @@
  // 100s -> Inserções no banco de dados
  // Sem os Streams seria necessário esperar 100 segundos para o node fazer as inserções no banco de dados. 
 
- //  10mb/s -> 10.000 
+ //  10mb/s -> 10.000 => Por que não ir inserindo os registros antes de todo o arquivo ser enviado. 
 
- // Readables Streams / Writable Streams
+ // Readables Streams / Writable Streams    
+
+ // Streams -> 
+
+// process.stdin
+// .pipe(process.stdout)
+
+import { Readable } from 'node:stream'
+
+class OneToHundredStream extends Readable {
+    index = 1
+    
+    _read(){
+        const i = this.index++
+
+        setTimeout(() => {
+            if ( i > 100){
+            this.push(null)
+        } else {
+            const buf = Buffer.from(String(i)) 
+
+            this.push(buf) 
+        }
+        }, 100)
+    }
+}
+
+// Com Streams você consegue trabalhar com os dados sem eles estarem completos.
+
+new OneToHundredStream()
+    .pipe(process.stdout)
