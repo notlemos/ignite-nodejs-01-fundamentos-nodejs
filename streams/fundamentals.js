dec73edcddbc479c3241120d00054d888c77ece1
notlemos <caiolemossfc123@hotmail.com>
2025-07-15 19:55:31 -0300

    Entendendo Streams no Node

---
+++ streams/fundamentals.js
@@ -0,0 +1,14 @@
+ // Netflix & Spotify
+
+ // Importação de clientes via CSV (excel)
+ // 1gb = 1.000.000
+ // POST /upload import.csv
+
+ // 10mb/s - 100s 
+
+ // 100s -> Inserções no banco de dados
+ // Sem os Streams seria necessário esperar 100 segundos para o node fazer as inserções no banco de dados. 
+
+ //  10mb/s -> 10.000 
+
+ // Readables Streams / Writable Streams
