659c6696b62d258951c2e0c4d4a25223fd741c96
notlemos <caiolemossfc123@hotmail.com>
2025-07-16 23:02:37 -0300

    Criando Middleware de JSON

---
+++ src/middlewares/json.js
@@ -0,0 +1,21 @@
+// middleware -> interceptador 
+// uma função que intercepta a requisição e é tratados.
+// middlewares sempre recebem req & res.
+
+export async function json(req, res) {
+
+    const buffers = []
+
+    for await (const chunk of req ) {
+        buffers.push(chunk)
+    }
+
+    try {
+        req.body = JSON.parse(Buffer.concat(buffers).toString()) 
+    } catch {
+        req.body = null 
+    }
+
+    res.setHeader('Content-type', 'application/json')
+
+}
