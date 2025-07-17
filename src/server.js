9829971554a8cf7d425f1f72beba9a10b3478979
notlemos <caiolemossfc123@hotmail.com>
2025-07-17 20:41:07 -0300

    Separando rotas da Aplicação

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import http from 'node:http'
-import { randomUUID } from 'node:crypto'
 import { json } from './middlewares/json.js'
 import { Database } from './database.js'
+import { routes } from './routes.js'
 
 
 // - Criar usuários
@@ -34,30 +34,19 @@ import { Database } from './database.js'
 
 // HTTP Status Code
 
-const database = new Database
+
 
 const server = http.createServer(async(req, res) => {
     const { method, url } = req 
 
     await json(req, res)
     
+    const route = routes.find(route => {
+        return route.method == method && route.path == url
+    })
 
-    if (method == "GET" && url == "/users"){
-
-        const users = database.select('users')
-
-        return res.end(JSON.stringify(users))  
-    }
-
-    if (method == "POST" && url == "/users"){
-        const { name, email } = req.body
-        const user = {
-            id: randomUUID(),
-            name,
-            email
-        }
-        database.insert('users', user)
-        return res.writeHead(201).end()
+    if (route) {
+        return route.handler(req, res)
     }
 
     return res.writeHead(404).end()
