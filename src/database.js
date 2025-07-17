c1373e4227d10a72dbf3408843bb6d2454abc3f9
notlemos <caiolemossfc123@hotmail.com>
2025-07-16 23:29:28 -0300

    Persistindo Banco de Dados

--- src/database.js
+++ src/database.js
@@ -1,7 +1,25 @@
+import fs from 'node:fs/promises'
 // {"users": [...]}, ...
+
+const databasePath = new URL('../db.json', import.meta.url)
+
+
 
 export class Database {
     #database = {}
+
+    constructor () {
+        fs.readFile(databasePath, 'utf8').then(data => {
+            this.#database = JSON.parse(data)
+        })
+        .catch(() => {
+            this.#persist()
+        })
+    }
+
+    #persist() {
+        fs.writeFile(databasePath, JSON.stringify(this.#database))
+    }
 
     select(table) {
         const data = this.#database[table] ?? []
@@ -15,6 +33,8 @@ export class Database {
         } else {
             this.#database[table] = [data]
         }
+
+        this.#persist();
 
         return data;
     }
