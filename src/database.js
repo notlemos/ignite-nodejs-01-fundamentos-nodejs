a83e8b8cda50dffd9968c7579c22af920147fb34
notlemos <caiolemossfc123@hotmail.com>
2025-07-17 23:19:21 -0300

    final

--- src/database.js
+++ src/database.js
@@ -21,8 +21,20 @@ export class Database {
         fs.writeFile(databasePath, JSON.stringify(this.#database))
     }
 
-    select(table) {
-        const data = this.#database[table] ?? []
+    select(table, search) {
+        let data = this.#database[table] ?? []
+
+        // let it change
+        // { name: "JohnDoe", email: "JohnDoe"}
+        // [ ['name', 'JohnDoe']' ['email', 'JohnDoe'] ]
+
+        if (search) {
+            data = data.filter(row => {
+                return Object.entries(search).some(([key, value]) => {
+                    return row[key].toLowerCase().includes(value.toLowerCase())
+                })
+            })
+        }
 
         return data
     }
